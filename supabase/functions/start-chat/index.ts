6430e2e8a0e773fe44daa19cce057e1bdac1f462
Tomáš Ščavnický <tomas.scavnicky@gmail.com>
2024-10-07 18:12:47 +0200

    init

---
+++ supabase/functions/start-chat/index.ts
@@ -0,0 +1,38 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+
+console.log("Hello from Functions!")
+
+Deno.serve(async (req) => {
+  const { message } = await req.json()
+
+  // TODO:
+  // create new chat
+  // store incoming message as the first message in the chat
+  // check if any new users/contacts were explicitly added to the initial chat message
+    // if no explicit users/contacts, look for implicit users by searching Google Places API
+    // store new users
+  // create system user (agent) based on the first chat message, i.e. voice agent on Vapi
+  // create new chats with new users, i.e. initiate new phone calls via Vapi
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/start-chat' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
